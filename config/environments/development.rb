e9802903421a70ce9f9a3a1c3cba66985159a5a7
maru <maru@marunoMacBook-Pro.local>
2023-02-25 16:52:46 +0900

    バグ修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Cannot render console from 172.18.0.1! 対策
+  config.web_console.allowed_ips = '0.0.0.0/0'
+
 end
